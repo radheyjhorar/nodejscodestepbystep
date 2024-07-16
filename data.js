d7ec0bcbe6f2c34d53511298e744f7f732289b50
radheyjhorar <shyamjhorar700@gmail.com>
2024-07-16 09:46:29 +0530

    half course is done

---
+++ data.js
@@ -0,0 +1,12 @@
+
+const data = [
+  {name:'Radhey Jhorar', email:'radheyjhorar@test.com'},
+  {name:'Jhorar', email:'jhorar@test.com'},
+  {name:'Radhey', email:'radhey@test.com'},
+  {name:'Raju', email:'raju@test.com'},
+  {name:'Ramesh', email:'ramesh@test.com'},
+  {name:'javesh', email:'javesh@test.com'},
+  {name:'Joravar', email:'joravar@test.com'},
+];
+
+module.exports = data;
--- index.js
+++ index.js
@@ -1,4 +1,137 @@
 
-// const colors = require('colors');
-const chalk = require('chalk');
-console.log(chalk.blue('Hello world!')); 
+// const http = require('http')
+// const data = require('./data');
+
+// http.createServer((req, res) => {
+//   res.writeHead(200, {"Content-Type": 'application/json'});
+//   res.write(JSON.stringify(data));
+//   res.end();
+// }).listen(5000);
+
+
+
+
+
+
+
+
+
+
+// const fs = require('fs');
+
+// const input = process.argv;
+
+// if(input[2] === 'add') {
+//   fs.writeFileSync(input[3], input[4])
+// } else if(input[2] === 'remove') {
+//   fs.unlinkSync(input[3])
+// } else {
+//   console.log("Invalid Output");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fs = require('fs');
+// const path = require('path');
+// const dirPath = path.join(__dirname, 'files');
+// console.log(dirPath);
+
+// for(let i = 0; i < 5; i++) {
+  // fs.writeFileSync(`${dirPath}/hello${i}.txt`, "A simple text file"); // create five new files
+  // fs.unlinkSync(`${dirPath}/hello${i}.txt`);  // remove the five files
+  // fs.writeFileSync(dirPath+"/test"+i+".txt", "A simple text file"); // create five new files
+  // fs.unlinkSync(dirPath+"/test"+i+".txt"); // remove the five files
+// }
+
+
+// fs.readdir(dirPath, (err, files) => {
+//   // console.log(files);
+//   files.forEach(file => console.log("File name is: " + file));
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// CRUD with file system
+// const fs = require("fs");
+// const path = require("path");
+// const dirPath = path.join(__dirname, "crud");
+// const filePath = `${dirPath}/apple.txt`
+
+// fs.writeFileSync(filePath, 'this is a simple text file');
+
+// fs.readFile(filePath, "utf8", (err, data) => {
+//   console.log(data);
+// })
+
+// fs.appendFile(filePath, ' and file name is apple.txt', err => !err && console.log("File is updated"));
+
+// fs.rename(filePath, `${dirPath}/fruit.txt`, (err) => {
+//   if(err) { 
+//     console.log(err);
+//   } else {
+//     console.log("File name is updated")
+//   }
+// });
+
+// fs.unlinkSync(`${dirPath}/fruit.txt`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let a = 10;
+let b = 0;
+
+
+let waitingData = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(20)
+  }, 2000);
+})
+
+waitingData.then((data) => {
+  b = data;
+  console.log(a + b);
+})
+
+
+console.log(a + b);
+
